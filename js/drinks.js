e4b076f3b49c425fb7db3dfcf8bbcfd1294b9d0f
Brenda Storer <brendastorer@gmail.com>
2014-08-14 22:09:13 -0400

    Original 2012 prototype

---
+++ js/drinks.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $(".block").toggle(function() {
+    $(this).animate({ backgroundColor: "#c33a89" }, 1500);
+    },function() {
+    $(this).animate({ backgroundColor: "#cc3012" }, 1500);
+    },function() {
+    $(this).animate({ backgroundColor: "#52a534" }, 1500);
+    },function() {
+    $(this).animate({ backgroundColor: "#724598" }, 1500);
+    },function() {
+    $(this).animate({ backgroundColor: "#0c71b7" }, 1500);
+    },function() {
+    $(this).animate({ backgroundColor: "#990b0d" }, 1500);
+    },function() {
+    $(this).animate({ backgroundColor: "#e5644f" }, 1500);
+    },function() {
+    $(this).animate({ backgroundColor: "#f47c08" }, 1500);
+  });
+});
