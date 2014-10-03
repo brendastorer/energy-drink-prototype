a30ff939034666f9cf7d2fe2e5500a200d7fe3e1
Brenda Storer <brendastorer@gmail.com>
2014-10-03 11:50:15 -0400

    Refactor for better semantics and styling.

--- js/drinks.js
+++ js/drinks.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var drinks = ["citrus-burst", "kiwi-strawberry", "triple-berry", "lemon-lime-rush", "grape-fusion", "blue-raspberry", "black-cherry", "wild-peach"];
   console.log (drinks [0]);
 
-  $(".block").toggle(function() {
+  $(".js-block").toggle(function() {
     $(this).animate({ backgroundColor: "#c33a89" }, 1500);
     },function() {
     $(this).animate({ backgroundColor: "#cc3012" }, 1500);
